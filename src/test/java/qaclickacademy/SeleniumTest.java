d12af844201b409be2a3cfc02802c1d243f1ad86
VusiAppium <vusi.ngwenya@absa.africa>
2020-12-31 11:55:57 +0200

    Sixth commit

--- src/test/java/qaclickacademy/SeleniumTest.java
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -1,4 +1,20 @@
 package qaclickacademy;
 
-public class SeleniumTest {
+import org.junit.jupiter.api.Test;
+
+public class SeleniumTest
+{
+    @Test
+    public void BrowserAUtomation()
+    {
+        System.out.println("BrowserAutomation");
+
+        System.out.println("Web automation");
+    }
+
+    @Test
+    public void elementsUi()
+    {
+        System.out.println("elementsUi");
+    }
 }
