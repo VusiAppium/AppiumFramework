6d6a1e82386ec17f52fccf2de5db206643ff8a0f
VusiAppium <vusi.ngwenya@absa.africa>
2020-12-31 12:42:24 +0200

    Architect Asian development

--- src/test/java/qaclickacademy/AppiumTest.java
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -1,4 +1,27 @@
 package qaclickacademy;
 
-public class AppiumTest {
+import org.junit.jupiter.api.Test;
+
+public class AppiumTest
+{
+    @Test
+    public void NativeAPpAndroid()
+    {
+        System.out.println("NativeAPpAndroid");
+        System.out.println("NativeAPpAndroid");
+    }
+
+    @Test
+    public void NativeAppAndroidS()
+    {
+        System.out.println("NativeAPpAndroid");
+        System.out.println("NativeAPpAndroid");
+    }
+
+    @Test
+    public void IOSApps()
+    {
+        System.out.println("IOSApps");
+        System.out.println("IOSApps");
+    }
 }
